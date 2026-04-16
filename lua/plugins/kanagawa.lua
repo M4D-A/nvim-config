105ac7e982545402f0077cee4af0be49fb1b0b8d
M4DA <jm4da@proton.me>
2026-04-16 22:39:51 +0200

    Kanagawa

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -5,44 +5,44 @@ return {
     config = function()
       require("kanagawa").setup {
         theme = "dragon",
-        -- transparent = true,
-        -- colors = {
-        --   theme = {
-        --     all = {
-        --       ui = {
-        --         bg_gutter = "none",
-        --       },
-        --     },
-        --   },
-        -- },
-        -- overrides = function(colors)
-        --   local theme = colors.theme
-        --   local palette = colors.palette
-        --   return {
-        --     NormalFloat = { bg = "none" },
-        --     FloatBorder = { bg = "none" },
-        --     FloatTitle = { bg = "none" },
-        --     Folded = { bg = "none" },
-        --     UfoFoldedEllipsis = { fg = palette.surimiOrange },
-        --     WinSeparator = { fg = theme.ui.float.fg_border },
-        --
-        --     TelescopeTitle = { fg = theme.ui.special, bold = true },
-        --     TelescopePromptBorder = { fg = theme.ui.float.fg_border, bg = "none" },
-        --     TelescopeResultsBorder = { fg = theme.ui.float.fg_border, bg = "none" },
-        --     TelescopePreviewBorder = { fg = theme.ui.float.fg_border, bg = "none" },
-        --
-        --     TabLine = { bg = "none" },
-        --     TabLineFill = { bg = "none" },
-        --     StatusLine = { bg = "none" },
-        --
-        --     LazyNormal = { bg = "none", fg = theme.ui.fg_dim },
-        --     MasonNormal = { bg = "none", fg = theme.ui.fg_dim },
-        --     Pmenu = { fg = theme.ui.shade0, bg = "none" }, -- add `blend = vim.o.pumblend` to enable transparency
-        --     PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-        --     PmenuSbar = { bg = theme.ui.bg_m1 },
-        --     PmenuThumb = { bg = theme.ui.bg_p2 },
-        --   }
-        -- end,
+        transparent = true,
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none",
+              },
+            },
+          },
+        },
+        overrides = function(colors)
+          local theme = colors.theme
+          local palette = colors.palette
+          return {
+            NormalFloat = { bg = "none" },
+            FloatBorder = { bg = "none" },
+            FloatTitle = { bg = "none" },
+            Folded = { bg = "none" },
+            UfoFoldedEllipsis = { fg = palette.surimiOrange },
+            WinSeparator = { fg = theme.ui.float.fg_border },
+
+            TelescopeTitle = { fg = theme.ui.special, bold = true },
+            TelescopePromptBorder = { fg = theme.ui.float.fg_border, bg = "none" },
+            TelescopeResultsBorder = { fg = theme.ui.float.fg_border, bg = "none" },
+            TelescopePreviewBorder = { fg = theme.ui.float.fg_border, bg = "none" },
+
+            TabLine = { bg = "none" },
+            TabLineFill = { bg = "none" },
+            StatusLine = { bg = "none" },
+
+            LazyNormal = { bg = "none", fg = theme.ui.fg_dim },
+            MasonNormal = { bg = "none", fg = theme.ui.fg_dim },
+            Pmenu = { fg = theme.ui.shade0, bg = "none" }, -- add `blend = vim.o.pumblend` to enable transparency
+            PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+            PmenuSbar = { bg = theme.ui.bg_m1 },
+            PmenuThumb = { bg = theme.ui.bg_p2 },
+          }
+        end,
       }
     end,
   },
